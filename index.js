6211a95f0d9326b16895706bf9e322839a2eff35
joshwyatt <joshuawyatt123@gmail.com>
2016-04-22 09:53:59 -0700

    Make timestamps relative to start time

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const fs = require( 'fs' );
 const path = require( 'path' );
 const readline = require( 'readline' );
 const eol = require( 'os' ).EOL;
-const moment = require( 'moment' );
-const now = moment();
 
 if ( process.argv.length !== 3 ) printUsageAndExit();
 
@@ -13,7 +11,15 @@ const FILENAME = `${process.argv[2]}.txt`;
 const FILEPATH = path.resolve( process.cwd(), './', FILENAME );
 
 const timestamp = function() {
-  return moment().format( 'hh:mm:ss A' );
+  let seconds = process.hrtime(time)[0];
+  return calculateTimestamp(seconds);
+}
+
+const calculateTimestamp = function(seconds) {
+  const minutes = Math.floor(seconds / 60).toString();
+  let remainingSeconds = (seconds % 60).toString();
+  let formatedSeconds = remainingSeconds.length === 1 ? remainingSeconds + '0' : remainingSeconds;
+  return `${minutes}:${formatedSeconds}`;
 }
 
 const rli = readline.createInterface({
@@ -21,8 +27,11 @@ const rli = readline.createInterface({
   output: fs.createWriteStream( FILEPATH, { flags: 'a' } )
 });
 
+let time = process.hrtime();
+
+
 rli.on( 'line', function ( line ) {
-  rli.output.write( timestamp() + ' > ' + String( line ) + eol );
+  rli.output.write( timestamp() + ' - ' + String( line ) + eol );
   process.stdout.write( '> ' );
 });
 
@@ -39,3 +48,4 @@ function printUsageAndExit () {
   console.log('Usage: timestamp <filename>');
   process.exit(1);
 }
+
